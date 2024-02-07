91a2520648614155a7b96932d7f981dde5cd0ce3
erdem abat <erdemabat@hotmail.com>
2024-02-07 20:32:12 +0300

    Add project files.

---
+++ Microservices.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,50 @@
+﻿using Microservices.Services.CouponAPI.Data;
+using Microservices.Services.CouponAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservices.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public CouponAPIController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _context.Coupons.ToList();
+                return objList;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public object Get(int id)
+        {
+            try
+            {
+                Coupon value = _context.Coupons.First(x=>x.CouponId == id);
+                return value;
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return null;
+        }
+    }
+}
