385e08fdb297afd9fb5e82db31fb462678085bdb
erdem abat <erdemabat@hotmail.com>
2024-02-08 15:22:16 +0300

    Added Coupon UI Base and Coupon services added

--- Microservices.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Microservices.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,7 +1,9 @@
-﻿using Microservices.Services.CouponAPI.Data;
+﻿using AutoMapper;
+using Microservices.Services.CouponAPI.Data;
 using Microservices.Services.CouponAPI.Models;
-using Microsoft.AspNetCore.Http;
+using Microservices.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Microservices.Services.CouponAPI.Controllers
 {
@@ -10,41 +12,114 @@ namespace Microservices.Services.CouponAPI.Controllers
     public class CouponAPIController : ControllerBase
     {
         private readonly AppDbContext _context;
-
-        public CouponAPIController(AppDbContext context)
+        private ResponseDto _response;
+        private IMapper _mapper;
+        public CouponAPIController(AppDbContext context, IMapper mapper)
         {
             _context = context;
+            _response = new ResponseDto();
+            _mapper = mapper;
         }
 
         [HttpGet]
-        public object Get()
+        public ResponseDto Get()
         {
             try
             {
                 IEnumerable<Coupon> objList = _context.Coupons.ToList();
-                return objList;
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(objList);
             }
             catch (Exception ex)
             {
-
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
-            return null;
+            return _response;
         }
 
         [HttpGet]
         [Route("{id:int}")]
-        public object Get(int id)
+        public ResponseDto Get(int id)
         {
             try
             {
-                Coupon value = _context.Coupons.First(x=>x.CouponId == id);
-                return value;
+                Coupon value = _context.Coupons.First(x => x.CouponId == id);
+                _response.Result = _mapper.Map<CouponDto>(value);
             }
             catch (Exception ex)
             {
-
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
             }
-            return null;
+            return _response;
+        }
+        [HttpGet]
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon value = _context.Coupons.First(x => x.CouponCode.ToLower() == code.ToLower());
+                _response.Result = _mapper.Map<CouponDto>(value);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPost]
+        public ResponseDto Create([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _context.Coupons.Add(obj);
+                _context.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpPut]
+        public ResponseDto Update([FromBody] CouponDto couponDto)
+        {
+            try
+            {
+                Coupon obj = _mapper.Map<Coupon>(couponDto);
+                _context.Coupons.Update(obj);
+                _context.SaveChanges();
+
+                _response.Result = _mapper.Map<CouponDto>(obj);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+        [HttpDelete]
+        public ResponseDto Delete(int id)
+        {
+            try
+            {
+                Coupon obj = _context.Coupons.First(x=>x.CouponId == id);
+                _context.Coupons.Remove(obj);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
         }
     }
 }
---
+++ Microservices.Web/Service/CouponService.cs
@@ -0,0 +1,72 @@
+﻿using Microservices.Web.Models;
+using Microservices.Web.Service.IService;
+using Microservices.Web.Utility;
+using static Microservices.Web.Utility.SD;
+
+namespace Microservices.Web.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+        public async Task<ResponseDto?> CreateCouponsAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.POST,
+                Data = couponDto,
+                Url = CouponAPIBase + "/api/CouponAPI/"
+            });
+        }
+
+        public async Task<ResponseDto?> DeleteCouponsAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.DELETE,
+                Url = CouponAPIBase + "/api/CouponAPI/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> GetAllCouponsAsync()
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = CouponAPIBase + "/api/CouponAPI"
+            });
+        }
+
+        public async Task<ResponseDto?> GetCouponAsync(string couponCode)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = CouponAPIBase + "/api/CouponAPI/GetBycode/" + couponCode
+            });
+        }
+
+        public async Task<ResponseDto?> GetCouponByIdAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.GET,
+                Url = CouponAPIBase + "/api/CouponAPI/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> UpdateCouponsAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = ApiType.PUT,
+                Data = couponDto,
+                Url = CouponAPIBase + "/api/CouponAPI/"
+            });
+        }
+    }
+}
